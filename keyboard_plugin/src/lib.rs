6d00f716e909364e49e6352a676b1e7c5d00bbe9
takotori <tranfelix99@gmail.com>
2024-04-08 16:27:59 +0200

    Add dragging of actionrow

--- keyboard_plugin/src/lib.rs
+++ keyboard_plugin/src/lib.rs
@@ -1,14 +1,11 @@
 use adw::{ActionRow, gdk};
-use adw::prelude::PreferencesGroupExt;
-use dbus::arg::messageitem::MessageItem::Str;
+use adw::prelude::{PreferencesGroupExt, PreferencesRowExt};
 use gdk4::ContentProvider;
 use glib::clone;
-use glib::gobject_ffi::GValue;
 use glib::prelude::StaticType;
-use gtk::Orientation;
+use gtk::{Label, Orientation, WidgetPaintable};
 use gtk::prelude::{BoxExt, ButtonExt, WidgetExt};
 use re_set_lib::utils::plugin::{PluginCapabilities, PluginImplementation, PluginTestFunc, SidebarInfo};
-use adw::prelude::PreferencesRowExt;
 
 pub const BASE: &str = "org.Xetibo.ReSet.Daemon";
 pub const DBUS_PATH: &str = "/org/Xebito/ReSet/Plugins/test";
@@ -41,76 +38,66 @@ pub extern "C" fn frontend_data() -> (SidebarInfo, Vec<gtk::Box>) {
     };
 
     let main = gtk::Box::builder().orientation(Orientation::Vertical).build();
-
-    let title = gtk::Label::builder()
-        .label("Keyboard")
-        .css_classes(vec!["resetSettingLabel"])
-        .halign(gtk::Align::Start)
-        .margin_start(5)
-        .margin_bottom(10)
-        .build();
-    main.append(&title);
+    main.append(&create_title());
 
     let keyboard_list = adw::PreferencesGroup::new();
     main.append(&keyboard_list);
 
-    let row = ActionRow::builder().title("test").build();
-    let row2 = ActionRow::builder().title("test2").build();
-    let row3 = ActionRow::builder().title("test3").build();
-    keyboard_list.add(&row);
-    keyboard_list.add(&row2);
-    keyboard_list.add(&row3);
+    // todo fetch keyboard layouts from somewhere ヽ(。_°)ノ
+
+    let layouts = vec![
+        ActionRow::builder().title("test").build(),
+        ActionRow::builder().title("test2").build(),
+        ActionRow::builder().title("test3").build(),
+    ];
+
+    for layout in layouts {
+        keyboard_list.add(&layout);
+        let source = gtk::DragSource::builder()
+            .actions(gdk::DragAction::MOVE)
+            .build();
 
 
-    let source = gtk::DragSource::builder()
-        .actions(gdk::DragAction::MOVE)
-        .build();
+        source.connect_prepare(clone!(@weak layout => @default-return None, move |value, x , y| {
+            dbg!("source Prepare {:?} and {:?} at ({}, {})", layout.title(), value, x, y);
+            let value1 = glib::Value::from(layout);
+            Some(ContentProvider::for_value(&value1))
+        }));
 
-    source.set_icon(Some(&gdk::Paintable::new_empty(100, 100)), 100, 100);
+        source.connect_drag_begin(clone!(@weak layout => move |value, x| {
+            dbg!("source Drag Begin {:?} at ({}, {})", value, x);
+            layout.add_css_class("selectedLanguage");
 
-    source.connect_prepare(clone!(@weak row => @default-return None, move |value, x , y| {
-        dbg!("source Prepare {:?} and {:?} at ({}, {})", row.title(), value, x, y);
-        let value1 = glib::Value::from("asldfks");
-        Some(ContentProvider::for_value(&value1))
-    }));
+            let paintable = WidgetPaintable::new(Some(&layout));
+            value.set_icon(Some(&paintable), 0, 0);
+        }));
 
-    // source.connect_prepare(move |value, x, y| {
-    //     dbg!("source Prepare {:?} at ({}, {})", value, x, y);
-    //     let value1 = glib::Value::from("asldfks");
-    //     Some(ContentProvider::for_value(&value1))
-    // });
+        source.connect_drag_end(clone!(@weak layout => move |value, x, y| {
+            dbg!("source Drag end {:?} at ({}, {})", value, x, y);
+            layout.remove_css_class("selectedLanguage");
+        }));
 
-    source.connect_drag_begin(move |value, x| {
-        dbg!("source Drag Begin {:?} at ({}, {})", value, x);
-    });
-
-    source.connect_drag_end(move |value, x, y| {
-        dbg!("source Drag end {:?} at ({}, {})", value, x, y);
-    });
-
-    let controller = gtk::EventController::from(source);
-    row.add_controller(controller);
+        let controller = gtk::EventController::from(source);
+        layout.add_controller(controller);
+    }
 
     let target = gtk::DropTarget::builder()
         .actions(gdk::DragAction::MOVE)
-        .formats(&gdk::ContentFormats::for_type(String::static_type()))
+        .formats(&gdk::ContentFormats::for_type(ActionRow::static_type()))
         .build();
 
     target.connect_drop(clone!(@weak keyboard_list => @default-return false, move |tar, value, x , y| {
-        dbg!("target Dropped {:?} at ({}, {})", value, x, y);
-        false
+        dbg!("target Dropped {:?} and {:?} at ({}, {})", tar, value, x, y);
+        // todo drop not working (╯°□°)╯︵ ┻━┻
+        let test = value.get::<ActionRow>();
+        keyboard_list.add(&test.unwrap());
+        true
     }));
 
     let controller = gtk::EventController::from(target);
     keyboard_list.add_controller(controller);
-    // main.add_controller(controller);
 
-
-    let boxes = vec![
-        main
-    ];
-
-    (info, boxes)
+    (info, vec![main])
 }
 
 #[no_mangle]
@@ -121,7 +108,17 @@ pub extern "C" fn frontend_tests() -> Vec<PluginTestFunc> {
 }
 
 pub struct LabelWrapper {
-    label: gtk::Label,
+    label: Label,
+}
+
+fn create_title() -> Label {
+    Label::builder()
+        .label("Keyboard")
+        .css_classes(vec!["resetSettingLabel"])
+        .halign(gtk::Align::Start)
+        .margin_start(5)
+        .margin_bottom(10)
+        .build()
 }
 
 unsafe impl Send for LabelWrapper {}
