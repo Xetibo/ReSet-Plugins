50f326d13203b311d8e18cff2ad43253d53f7ea8
Fabio Lenherr / DashieTM <fabio.lenherr@gmail.com>
2024-04-09 17:40:31 +0200

    chore: reinitialize repository

---
+++ monitors/src/const.rs
@@ -0,0 +1,5 @@
+pub const BASE: &str = "org.Xetibo.ReSet.Daemon";
+pub const DBUS_PATH: &str = "/org/Xebito/ReSet/Plugins/Monitors";
+pub const INTERFACE: &str = "org.Xetibo.ReSet.Monitors";
+
+pub const SUPPORTED_ENVIRONMENTS: [&str; 1] = ["Hyprland"];
