68a4cea0ae378576d9a13d5b9e4635e89e45a73b
takotori <tranfelix99@gmail.com>
2024-04-12 12:12:38 +0200

    finish drag and drop for keyboard plugin

--- keyboard_plugin/src/lib.rs
+++ keyboard_plugin/src/lib.rs
@@ -2,20 +2,19 @@ use adw::{ActionRow, gdk};
 use adw::prelude::{PreferencesGroupExt, PreferencesRowExt};
 use gdk4::ContentProvider;
 use glib::clone;
-use glib::prelude::StaticType;
-use gtk::{Label, Orientation, WidgetPaintable};
-use gtk::prelude::{BoxExt, ButtonExt, WidgetExt};
+use gtk::{Align, EventController, Label, ListBox, Orientation, WidgetPaintable};
+use gtk::{DragSource, prelude::*};
 use re_set_lib::utils::plugin::{PluginCapabilities, PluginImplementation, PluginTestFunc, SidebarInfo};
 
 pub const BASE: &str = "org.Xetibo.ReSet.Daemon";
-pub const DBUS_PATH: &str = "/org/Xebito/ReSet/Plugins/test";
-pub const INTERFACE: &str = "org.Xetibo.ReSet.TestPlugin";
+pub const DBUS_PATH: &str = "/org/Xebito/ReSet/Plugins/Keyboard";
+pub const INTERFACE: &str = "org.Xetibo.ReSet.Keyboard";
 
 #[no_mangle]
 #[allow(improper_ctypes_definitions)]
 pub extern "C" fn capabilities() -> PluginCapabilities {
     println!("frontend capabilities called");
-    PluginCapabilities::new(vec!["frontend test"], PluginImplementation::Frontend)
+    PluginCapabilities::new(vec!["Keyboard"], PluginImplementation::Frontend)
 }
 
 #[no_mangle]
@@ -40,11 +39,19 @@ pub extern "C" fn frontend_data() -> (SidebarInfo, Vec<gtk::Box>) {
     let main = gtk::Box::builder().orientation(Orientation::Vertical).build();
     main.append(&create_title());
 
-    let keyboard_list = adw::PreferencesGroup::new();
+    let keyboard_list = adw::PreferencesGroup::builder()
+        .title("Keyboard Layouts")
+        .description("Includes keyboard layouts and input methods")
+        .build();
     main.append(&keyboard_list);
 
-    // todo fetch keyboard layouts from somewhere ヽ(。_°)ノ
+    let add_layout_button = gtk::Button::builder()
+        .icon_name("value-increase-symbolic")
+        .valign(Align::Start)
+        .build();
+    keyboard_list.set_header_suffix(Some(&add_layout_button));
 
+    // todo fetch keyboard layouts from somewhere ヽ(。_°)ノ
     let layouts = vec![
         ActionRow::builder().title("test").build(),
         ActionRow::builder().title("test2").build(),
@@ -53,51 +60,59 @@ pub extern "C" fn frontend_data() -> (SidebarInfo, Vec<gtk::Box>) {
 
     for layout in layouts {
         keyboard_list.add(&layout);
-        let source = gtk::DragSource::builder()
+        let source = DragSource::builder()
             .actions(gdk::DragAction::MOVE)
             .build();
 
-
-        source.connect_prepare(clone!(@weak layout => @default-return None, move |value, x , y| {
-            dbg!("source Prepare {:?} and {:?} at ({}, {})", layout.title(), value, x, y);
-            let value1 = glib::Value::from(layout);
-            Some(ContentProvider::for_value(&value1))
+        source.connect_prepare(clone!(@weak layout => @default-return None, move |_, _, _| {
+            let value = glib::Value::from(layout);
+            Some(ContentProvider::for_value(&value))
         }));
 
-        source.connect_drag_begin(clone!(@weak layout => move |value, x| {
-            dbg!("source Drag Begin {:?} at ({}, {})", value, x);
+        source.connect_drag_begin(clone!(@weak layout, @weak keyboard_list => move |value, _| {
             layout.add_css_class("selectedLanguage");
 
             let paintable = WidgetPaintable::new(Some(&layout));
             value.set_icon(Some(&paintable), 0, 0);
         }));
 
-        source.connect_drag_end(clone!(@weak layout => move |value, x, y| {
-            dbg!("source Drag end {:?} at ({}, {})", value, x, y);
+        source.connect_drag_end(clone!(@weak layout => move |_, _, _| {
             layout.remove_css_class("selectedLanguage");
         }));
 
-        let controller = gtk::EventController::from(source);
-        layout.add_controller(controller);
+        let target = gtk::DropTarget::builder()
+            .actions(gdk::DragAction::MOVE)
+            .formats(&gdk::ContentFormats::for_type(ActionRow::static_type()))
+            .build();
+
+        target.connect_drop(clone!(@weak keyboard_list => @default-return false, move |target, value, _ , _| {
+            let selected_row = value.get::<ActionRow>().unwrap();
+            let droptarget_row = target.widget();
+            let droptarget_row = droptarget_row.downcast_ref::<ActionRow>().unwrap();
+            
+            let listbox = droptarget_row.parent().unwrap();
+            let listbox = listbox.downcast_ref::<ListBox>().unwrap();
+            
+            if droptarget_row.title() != selected_row.title() {
+                let index = droptarget_row.index();
+                keyboard_list.remove(&selected_row);
+                listbox.insert(&selected_row, index);
+                update_input();
+                return true;
+            }
+
+            false
+        }));
+
+        layout.add_controller(EventController::from(source));
+        layout.add_controller(EventController::from(target));
     }
 
-    let target = gtk::DropTarget::builder()
-        .actions(gdk::DragAction::MOVE)
-        .formats(&gdk::ContentFormats::for_type(ActionRow::static_type()))
-        .build();
-
-    target.connect_drop(clone!(@weak keyboard_list => @default-return false, move |tar, value, x , y| {
-        dbg!("target Dropped {:?} and {:?} at ({}, {})", tar, value, x, y);
-        // todo drop not working (╯°□°)╯︵ ┻━┻
-        let test = value.get::<ActionRow>();
-        keyboard_list.add(&test.unwrap());
-        true
-    }));
-
-    let controller = gtk::EventController::from(target);
-    keyboard_list.add_controller(controller);
-
     (info, vec![main])
+}
+
+fn update_input() {
+    dbg!("apply input order");
 }
 
 #[no_mangle]
@@ -105,10 +120,6 @@ pub extern "C" fn frontend_data() -> (SidebarInfo, Vec<gtk::Box>) {
 pub extern "C" fn frontend_tests() -> Vec<PluginTestFunc> {
     println!("frontend tests called");
     vec![]
-}
-
-pub struct LabelWrapper {
-    label: Label,
 }
 
 fn create_title() -> Label {
@@ -120,7 +131,3 @@ fn create_title() -> Label {
         .margin_bottom(10)
         .build()
 }
-
-unsafe impl Send for LabelWrapper {}
-
-unsafe impl Sync for LabelWrapper {}
