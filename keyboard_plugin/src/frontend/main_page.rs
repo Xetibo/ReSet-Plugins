871907ef6a5c0ccbc3904bd903ea37e45bb11b70
takotori <tranfelix99@gmail.com>
2024-04-21 12:58:38 +0200

    parse command output refactoring

---
+++ keyboard_plugin/src/frontend/main_page.rs
@@ -0,0 +1,71 @@
+use adw::{ActionRow, NavigationPage, NavigationView, PreferencesGroup};
+use adw::gio::{ActionEntry, SimpleActionGroup};
+use adw::prelude::{PreferencesGroupExt};
+use glib::{clone, Variant, VariantTy};
+use gtk::{Align, Button, Orientation};
+use gtk::prelude::*;
+
+use crate::backend::get_saved_layouts;
+use crate::frontend::{add_listener, update_input};
+
+pub fn create_keyboard_main_page(nav_view: &NavigationView) {
+    let mut user_layouts = get_saved_layouts();
+
+    let front_page_box = &gtk::Box::new(Orientation::Vertical, 0);
+    let front_page = NavigationPage::builder()
+        .tag("main")
+        .child(front_page_box)
+        .build();
+    nav_view.add(&front_page);
+
+    let keyboard_list = PreferencesGroup::builder()
+        .title("Keyboard Layouts")
+        .description("Includes keyboard layouts and input methods")
+        .build();
+    front_page_box.append(&keyboard_list);
+
+    let entry2 = ActionEntry::builder("changeorder")
+        .parameter_type(Some(&VariantTy::TUPLE))
+        .activate(clone!(@strong user_layouts => move |_, _, description| {
+            let (from, to) = description.unwrap().get::<(String, String)>().unwrap();
+            let layout = user_layouts.iter().find(|x| x.description == from).unwrap();
+            
+            // let from_index = user_layouts.iter().position(|x| x.description == from).unwrap();
+            // user_layouts.remove(from_index);
+            // 
+            // let to_index = user_layouts.iter().position(|x| x.description == to).unwrap();
+            // user_layouts.insert(to_index, layout.clone());
+            
+            dbg!(from, to);
+            // todo do stuff to list
+            update_input(&user_layouts);
+        }))
+        .build();
+
+    let action_group = SimpleActionGroup::new();
+    let entry = ActionEntry::builder("addlayout")
+        .parameter_type(Some(&String::static_variant_type()))
+        .activate(clone!(@weak keyboard_list, @strong user_layouts => move |_, _, description| {
+            let layout_description = description.unwrap().str().unwrap();
+            let layout_row = ActionRow::builder().title(layout_description).build();
+            keyboard_list.add(&layout_row);
+            add_listener(&keyboard_list, layout_row);
+            update_input(&user_layouts);
+        }))
+        .build();
+    action_group.add_action_entries([entry, entry2]);
+    nav_view.insert_action_group("keyboard", Some(&action_group));
+
+    let add_layout_button = Button::builder()
+        .icon_name("value-increase-symbolic")
+        .valign(Align::Start)
+        .build();
+    keyboard_list.set_header_suffix(Some(&add_layout_button));
+    add_layout_button.set_action_name(Some("navigation.push"));
+    add_layout_button.set_action_target_value(Some(&Variant::from("add_keyboard")));
+
+    for layout in user_layouts {
+        let layout_row = ActionRow::builder().title(layout.description.clone()).build();
+        add_listener(&keyboard_list, layout_row);
+    }
+}
