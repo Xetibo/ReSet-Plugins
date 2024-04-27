4363ad3f487b34346fd2c2fcda85598990b30dfa
takotori <tranfelix99@gmail.com>
2024-04-27 14:53:01 +0200

    color active keyboard layouts depending on environment

--- keyboard_plugin/src/lib.rs
+++ keyboard_plugin/src/lib.rs
@@ -1,9 +1,12 @@
+use std::time::Duration;
 use adw::NavigationView;
+use dbus::blocking::Connection;
+use dbus::Error;
 use gtk::{Box, Orientation};
 use gtk::prelude::*;
 use re_set_lib::utils::plugin::{PluginCapabilities, PluginImplementation, PluginTestFunc, SidebarInfo};
 
-use crate::frontend::{create_title, get_keyboard_list_frontend};
+use crate::frontend::{create_title};
 use crate::frontend::add_layout_page::create_add_keyboard_page;
 use crate::frontend::main_page::create_keyboard_main_page;
 
--- keyboard_plugin/src/utils.rs
+++ keyboard_plugin/src/utils.rs
@@ -1,4 +1,8 @@
 use std::process::Output;
+use std::time::Duration;
+use dbus::blocking::Connection;
+use dbus::Error;
+use crate::r#const::{BASE, DBUS_PATH, INTERFACE};
 
 pub fn parse_setting(command_output: Output) -> Vec<String> {
     let output = String::from_utf8(command_output.stdout).expect("not utf8");
@@ -17,4 +21,22 @@ pub fn get_default_path() -> String {
         .to_str()
         .unwrap();
     String::from(path)
+}
+
+pub fn get_environment() -> String {
+    let desktop = std::env::var("XDG_CURRENT_DESKTOP");
+    if desktop.is_err() {
+        return "NONE".into();
+    }
+    desktop.unwrap()
+}
+
+pub fn get_max_active_keyboards() -> u32 {
+    let conn = Connection::new_session().unwrap();
+    let proxy = conn.with_proxy(BASE, DBUS_PATH, Duration::from_millis(1000));
+    let res: Result<(u32, ), Error> = proxy.method_call(INTERFACE, "GetMaxActiveKeyboards", ());
+    if res.is_err() {
+        return 0;
+    }
+    res.unwrap().0
 }
