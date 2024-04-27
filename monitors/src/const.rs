a4838c782658e5c8c8ce4ba3962dd58393d48c93
DashieTM <fabio.lenherr@gmail.com>
2024-04-27 16:45:43 +0200

    monitor: Add gnome as supported env

--- monitors/src/const.rs
+++ monitors/src/const.rs
@@ -2,4 +2,4 @@ pub const BASE: &str = "org.Xetibo.ReSet.Daemon";
 pub const DBUS_PATH: &str = "/org/Xebito/ReSet/Plugins/Monitors";
 pub const INTERFACE: &str = "org.Xetibo.ReSet.Monitors";
 
-pub const SUPPORTED_ENVIRONMENTS: [&str; 1] = ["Hyprland"];
+pub const SUPPORTED_ENVIRONMENTS: [&str; 2] = ["Hyprland", "GNOME"];
