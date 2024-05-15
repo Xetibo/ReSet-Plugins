87be1f2e0c7a005d64f4e530015c89bbf263de09
DashieTM <fabio.lenherr@gmail.com>
2024-05-16 01:28:37 +0200

    monitor: kde protocol -> try fetching eventsga

--- monitors/src/backend/kde2.rs
+++ monitors/src/backend/kde2.rs
@@ -327,7 +327,7 @@ pub fn kde2_get_monitor_information() -> Vec<Monitor> {
         }
     }
     // queue.blocking_dispatch(&mut data).unwrap();
-    // queue.roundtrip(&mut data).unwrap();
+    queue.roundtrip(&mut data).unwrap();
     // for i in 0..5 {
     //     if what.is_ok() {
     //         what.unwrap();
