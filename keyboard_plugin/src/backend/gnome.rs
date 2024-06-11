7f04e8f08fa97d6066cbe77fbd72e194bcca0f75
takotori <tranfelix99@gmail.com>
2024-06-11 12:25:32 +0200

    use getenv to check if flatpak

--- keyboard_plugin/src/backend/gnome.rs
+++ keyboard_plugin/src/backend/gnome.rs
@@ -1,7 +1,6 @@
 use std::process::Command;
 
-use glib::{Variant, VariantTy};
-use gtk::prelude::SettingsExtManual;
+use glib::{getenv, Variant, VariantTy};
 #[cfg(debug_assertions)]
 use re_set_lib::{utils::macros::ErrorLevel, write_log_to_file};
 use re_set_lib::ERROR;
@@ -11,31 +10,32 @@ use crate::keyboard_layout::KeyboardLayout;
 pub fn get_saved_layouts_gnome(all_keyboards: &[KeyboardLayout]) -> Vec<KeyboardLayout> {
     let mut kb = vec![];
 
-    let mut result = Command::new("dconf")
-        .args(&["read", "/org/gnome/desktop/input-sources/sources"])
-        .output();
-    if result.is_err() {
-        return kb;
-    }
-    let mut output = result.unwrap();
-    let mut layout_variant = String::from_utf8(output.stdout).unwrap();
-
-    if layout_variant.contains("@a(ss)") {
+    let result;
+    if getenv("container").is_none() {
+        dbg!("not flatpak");
+        result = Command::new("dconf")
+            .args(&["read", "/org/gnome/desktop/input-sources/sources"])
+            .output();
+    } else {
+        dbg!("flatpak");
         result = Command::new("flatpak-spawn")
             .args(&["--host", "dconf", "read", "/org/gnome/desktop/input-sources/sources"])
             .output();
-        if result.is_err() {
-            return kb;
-        }
-        output = result.unwrap();
-        layout_variant = String::from_utf8(output.stdout).unwrap();
     }
+    if result.is_err() {
+        dbg!("result error");
+        return kb;
+    }
+    let output = result.unwrap();
+    let layout_variant = String::from_utf8(output.stdout).unwrap();
 
     let layout_variant = Variant::parse(Some(&VariantTy::new("a(ss)").unwrap()), &layout_variant);
     if layout_variant.is_err() {
+        dbg!("couldnt parse layout variant");
         return kb;
     }
     let layout_variant = layout_variant.unwrap();
+    dbg!(&layout_variant);
     let layouts = layout_variant.get::<Vec<(String, String)>>().unwrap();
     for layout in layouts {
         let kb_layout: Vec<&KeyboardLayout> = if layout.1.contains("+") {
@@ -59,11 +59,11 @@ pub fn get_saved_layouts_gnome(all_keyboards: &[KeyboardLayout]) -> Vec<Keyboard
     kb
 }
 
-pub fn write_to_config_gnome(layouts: Vec<KeyboardLayout>) {
+pub fn write_to_config_gnome(layouts: &Vec<KeyboardLayout>) {
     let mut all_layouts = vec![];
     for x in layouts {
         let mut layout_string = format!("('xkb', '{}", x.name.clone());
-        if let Some(var) = x.variant {
+        if let Some(var) = &x.variant {
             layout_string += &format!("+{}", var);
         }
         layout_string += "')";
@@ -74,27 +74,21 @@ pub fn write_to_config_gnome(layouts: Vec<KeyboardLayout>) {
     all_layouts.insert_str(0, "[");
     all_layouts.push_str("]");
 
-    let result = Command::new("dconf")
-        .args(&["read", "/org/gnome/desktop/input-sources/sources"])
-        .output();
-    if result.is_err() {
-        return;
-    }
-    let output = result.unwrap();
-    let layout_variant = String::from_utf8(output.stdout).unwrap();
-
-    if layout_variant.contains("@a(ss)") {
-        let result = Command::new("flatpak-spawn")
-            .args(&["--host", "dconf", "write", "/org/gnome/desktop/input-sources/sources", all_layouts.as_str()])
-            .output();
-        if result.is_err() {
-            ERROR!("Failed to write layouts", ErrorLevel::PartialBreakage);
-        }
-    } else { 
+    if getenv("container").is_none() {
+        dbg!("not flatpak");
         let result = Command::new("dconf")
             .args(&["write", "/org/gnome/desktop/input-sources/sources", all_layouts.as_str()])
             .output();
         if result.is_err() {
+            ERROR!("Failed to write layouts", ErrorLevel::PartialBreakage);
+        }
+    } else {
+        dbg!("flatpak");
+        let result = Command::new("flatpak-spawn")
+            .args(&["--host", "dconf", "write", "/org/gnome/desktop/input-sources/sources", all_layouts.as_str()])
+            .output();
+        if result.is_err() {
+            dbg!("Failed to write with flatpak");
             ERROR!("Failed to write layouts", ErrorLevel::PartialBreakage);
         }
     }
